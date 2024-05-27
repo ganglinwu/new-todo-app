4613f20892e7428a55b5ef87b3feb820b9b15ff5
ganglinwu <ganglin.wu@gmail.com>
2024-05-27 17:15:58 +0800

    sidebar with demoData

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -4,7 +4,10 @@ export default function Header() {
   return (
     <div className="grid grid-cols-3 md:grid-cols-4 pt-4 md:pt-6">
       <div className="flex justify-center items-center">
-        <img src={logo} className="aspect-square w-24 shadow-xl col-span-1" />
+        <img
+          src={logo}
+          className="aspect-square w-24 md:w-[70%] shadow-xl col-span-1"
+        />
       </div>
       <div className="self-center md:col-span-3 col-span-2">Legend</div>
     </div>
--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -1,14 +1,30 @@
+import { ListPlus } from "lucide-react";
 import { ScrollArea } from "../../@/components/ui/scroll-area.tsx";
-import { Separator } from "../../@/components/ui/separator.tsx";
+
+import { demoProjects } from "../demoData/demoProjects.ts";
 
 export default function Sidebar() {
   return (
-    <div className="p-4">
-      <ScrollArea className="shadow-xl border border-blue-300 rounded-l p-2">
-        <h4>Project 1</h4>
-        <Separator></Separator>
-        <h4>Project 2</h4>
-        <Separator></Separator>
+    <div className="p-4 md:p-6 lg:p-8">
+      <ScrollArea className="shadow-xl border border-border rounded-l">
+        <div className="hover:bg-secondary flex justify-between p-2">
+          <h4 className="font-light text-xs md:text-lg lg:text-2xl self-center">
+            Add Project
+          </h4>
+          <button
+            type="button"
+            className="flex items-center aspect-square w-5 md:w-8 lg:w-10"
+          >
+            <ListPlus></ListPlus>
+          </button>
+        </div>
+        <div className="bg-secondary">
+          {demoProjects.map((project) => (
+            <h4 className="font-semibold p-2 text-xs md:text-lg md:p-4 lg:text-2xl lg-p-6 self-center">
+              {project.projectName}
+            </h4>
+          ))}
+        </div>
       </ScrollArea>
     </div>
   );
