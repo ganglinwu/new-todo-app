a6e6f096d8ae75b135db90c0957cc1cd8e11418e
ganglinwu <ganglin.wu@gmail.com>
2024-07-25 10:15:11 +0800

    refactor(initializeUserData): remove debug console.log

--- src/utils/initializeUserData.ts
+++ src/utils/initializeUserData.ts
@@ -7,21 +7,17 @@ export function initializeUserData(): userData {
   if (storageAvailable("localStorage")) {
     const dataString = localStorage.getItem("userData");
     if (dataString) {
-      console.log("dataString exists");
       const localStorageUserData = dateParser(JSON.parse(dataString));
-      console.log("printing parsed localStorageUserData");
-      console.log(localStorageUserData);
       const timeDiff =
         localStorageUserData.timeUpdated.getTime() -
         demoUserData.timeUpdated.getTime();
-      console.log(`printing time diff between local and demo data ${timeDiff}`);
       if (timeDiff > 0) {
         return localStorageUserData;
       } else {
         return demoUserData;
       }
     } else {
-      console.log("data string does not exist");
+      prompt("data string does not exist");
     }
   } else {
     prompt(
