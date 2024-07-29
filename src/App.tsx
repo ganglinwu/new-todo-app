c19ff69445f0a78512b25705c3666429a34051ef
ganglinwu <ganglin.wu@gmail.com>
2024-07-29 15:07:04 +0800

    feat(auth and login):
    - checks if user is auth or logged in
    - opens log in page if not logged in
    
    To fix:
    - however unable to change auth state, might need to pass in setAuth as
      props to login component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@
 import Header from "./layout/header/Header.tsx";
 import MainContent from "./layout/maincontent/MainContent.tsx";
 import Sidebar from "./layout/sidebar/Sidebar.tsx";
+import Loading from "./layout/loadingpage/Loading.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
 
 import { useState, useEffect } from "react";
 import { initializeUserData } from "./utils/initializeUserData.ts";
@@ -10,6 +12,11 @@ function App() {
   const [selectedProject, setSelectedProject] = useState("All Projects");
   const [userData, setUserData] = useState(() => initializeUserData());
   const [projects, setProjects] = useState(userData.projects);
+  const [isLoading, setIsLoading] = useState(false);
+  const [token, setToken] = useState(); //JWT
+  const [isAuth, setIsAuth] = useState(false);
+  const [isLoggedin, setIsLoggedIn] = useState(false);
+
   useEffect(() => {
     let newUserData = {
       userName: userData.userName,
@@ -19,26 +26,74 @@ function App() {
     setUserData(newUserData);
     localStorage.setItem("userData", JSON.stringify(newUserData));
   }, [projects]);
+
+  useEffect(() => {
+    console.log("useEffect for isLoggedin isAuth check triggered");
+    fetch(`http://localhost:3001/auth`, {
+      method: "POST",
+      credentials: "include",
+    }).then((response) => {
+      if (response.ok) {
+        () => console.log("response from server: authorized");
+        () => setIsAuth(true);
+        () => setIsLoggedIn(true);
+      } else {
+        setIsAuth(false);
+        setIsLoggedIn(false);
+      }
+    });
+  }, [isLoggedin, isAuth]);
+
+  // TODO: DB read one-time. also think about how to integrate with localStorage
+  // useEffect(()=> {
+  //   setIsLoading(true)
+  //   const fetchFromDB = async (){
+  //     try {
+  //     const res = await fetch("")
+  //     const data = await res.json()
+  //     // typecheck data from DB
+  //     if (typeof data === userData) {
+  //     setUserData(data)
+  // } catch (err) {
+  // return <div>something went wrong<div>
+  // }
+  //     }
+  //   }
+  //   setIsLoading(false)
+  // }, [])
+  //
+  //TODO: cache DB read?
+  // TODO: DB write?
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+
   return (
-    <div className="flex flex-col max-h-screen">
-      <Header></Header>
-      <div className="grid grid-cols-[40%_60%] md:grid-cols-[20%_80%] overflow-auto whitespace-nowrap">
-        <Sidebar
-          projects={projects}
-          selectedProject={selectedProject}
-          onSelect={setSelectedProject}
-          setProjects={setProjects}
-          userData={userData}
-          setUserData={setUserData}
-        ></Sidebar>
-        <div className="sticky top-0 z-10">
-          <MainContent
-            project={projects}
-            selectedProject={selectedProject}
-          ></MainContent>
+    <>
+      {!isLoggedin && <LoginPage setIsLoggedIn={setIsLoggedIn} />}
+      {isAuth && (
+        <div className="flex flex-col max-h-screen">
+          <Header></Header>
+          <div className="grid grid-cols-[40%_60%] md:grid-cols-[20%_80%] overflow-auto whitespace-nowrap">
+            <Sidebar
+              projects={projects}
+              selectedProject={selectedProject}
+              onSelect={setSelectedProject}
+              setProjects={setProjects}
+              userData={userData}
+              setUserData={setUserData}
+            ></Sidebar>
+            <div className="sticky top-0 z-10">
+              <MainContent
+                project={projects}
+                selectedProject={selectedProject}
+              ></MainContent>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
