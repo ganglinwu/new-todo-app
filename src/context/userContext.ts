fd7875bf0f748ab8e875476ef9bad007fda03437
ganglinwu <ganglin.wu@gmail.com>
2024-08-28 21:55:56 +0800

    refactor: userData type exports moved to userContext

--- src/context/userContext.ts
+++ src/context/userContext.ts
@@ -20,7 +20,33 @@ export const UserContext = createContext<userContextType>({
   logout: () => {},
   isAuth: false,
   setIsAuth: () => {},
+  userData: {
+    userName: "",
+  },
+  setUserData: () => {},
 });
 
-//TODO: zod object of userData shape
-//TODO: userData to be included in UserContext
+export const taskSchema = z.object({
+  taskName: z.string().nonempty(),
+  taskDueDate: z.date().optional(),
+  taskDuration: z.number().optional(),
+  taskUrgency: z.union([
+    z.literal("Low"),
+    z.literal("Medium"),
+    z.literal("High"),
+  ]),
+});
+
+export const projectSchema = z.object({
+  projectName: z.string().nonempty(),
+  tasks: taskSchema.array().optional(),
+});
+
+export const userDataSchema = z.object({
+  userName: z.string().nonempty(),
+  projects: projectSchema.array().optional(),
+});
+
+export type userData = z.infer<typeof userDataSchema>;
+export type projects = z.infer<typeof projectSchema>;
+export type tasks = z.infer<typeof taskSchema>;
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,20 +1,3 @@
-export type task = {
-  taskName: string;
-  taskDueDate?: Date;
-  taskDuration?: number;
-  taskUrgency: "Low" | "Medium" | "High";
-};
-
-export type projects = {
-  projectName: string;
-  tasks?: task[];
-};
-
-export type userData = {
-  userName: string;
-  projects?: projects[];
-};
-
 // types for JSON parsed localStorage i.e. coerced strings
 export type parsedTask = {
   taskName: string;
