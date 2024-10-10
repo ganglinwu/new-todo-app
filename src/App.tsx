f9453828d6d3db1a98375aa9ba7f8e000839f78f
ganglinwu <ganglin.wu@gmail.com>
2024-10-10 12:13:49 +0800

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
-import { useLocation, redirect, Routes, Route } from "react-router-dom";
+import {
+  useLocation,
+  useNavigate,
+  redirect,
+  Route,
+  Routes,
+} from "react-router-dom";
 import Index from "./pages/Index";
 import LoginPage from "./pages/LoginPage";
 import Error404 from "./pages/Error404";
@@ -18,10 +24,8 @@ export default function App() {
   });
 
   const [isAuth, setIsAuth] = useState(false);
-
-  const [isAuth, setIsAuth] = useState(false);
-  // const location = useLocation();
-
+  const navigate = useNavigate();
+  const location = useLocation();
 
   const login = async (username: string, password: string) => {
     const res = await fetch("http://localhost:3001/login", {
@@ -55,7 +59,7 @@ export default function App() {
     if (res.ok) {
       setUser({ username: null, email: null });
       redirect("/login");
-      setIsAuth(false)
+      setIsAuth(false);
       console.log("logged out");
     } else {
       alert("logout failed, do try again later");
